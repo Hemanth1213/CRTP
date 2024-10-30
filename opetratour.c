64f57b859184a5dcf6e8aff6ace9e9a0c6bef540
Hemanth1213 <atmurihemanth@gmail.com>
2024-10-30 15:50:25 +0530

    Create opetratour.c

---
+++ opetratour.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main()
+{
+char ch;
+printf("enter the characters");
+scanf("%c",&ch);
+switch(ch)
+{
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    case 'A':
+    case 'E':
+    case 'I':
+    case 'O':
+    case 'U':printf("enter vowel characters%c",);
+    default:printf("it is invalid");
+    break;
+}
+}
