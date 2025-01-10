044960cdf40a0279eacf137d55e1a3a72e6f70ad
Hemanth1213 <atmurihemanth@gmail.com>
2025-01-10 11:18:52 +0530

    Create To read the elements of an array .c

---
+++ To read the elements of an array .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a[10],i,n;
+    printf("enter the arry size:");
+    scanf("%d",&n);
+    printf("enter the array elements:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d\t",a[i]);
+        
+    }
+    printf("\n");
+}
